765d0c3e76e056d8f8d9c4cfa05e007aec55bd57
amalianurulrahmawati <92409385+amalianurulrahmawati@users.noreply.github.com>
2021-11-28 19:10:24 +0700

    Add files via upload

---
+++ 2117051008_Amalia Nurul Rahmawati_pretest2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	string berat_badan;
+	string tinggi_badan;
+	
+	//input
+	cout<<"Data Dibawah Ini Silahkan Di Isi"<<endl;
+	cout<<"Berat Badan : 59 "<<berat_badan<<endl;
+	cout<<"Dan Tinggi Badan : 1.7"<<tinggi_badan<<endl;
+	
+	
+	//output
+	cout<<"Hasil BMI "<<endl;
+	cout<<"BMI :"<< 59/1.7 <<endl;
+	
+	return 0;
+}
